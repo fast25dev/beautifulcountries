325a4d3b8616a75259d7fe0c7f25f21982eff3da
bogibekdev <bogibekdev@gmail.com>
2023-09-24 16:53:20 +0500

    ui finished

--- app/src/main/java/dev/fast25/beautifulcountries/ui/DetailActivity.kt
+++ app/src/main/java/dev/fast25/beautifulcountries/ui/DetailActivity.kt
@@ -3,6 +3,7 @@ package dev.fast25.beautifulcountries.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
+import dev.fast25.beautifulcountries.R
 import dev.fast25.beautifulcountries.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -17,9 +18,12 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        loading = findViewById(R.id.loading)
+        internet = findViewById(R.id.noInternet)
         name = intent.getStringExtra("name") ?: "USA"
+
         binding.apply {
-            tvName.text = name
+            tvCountryName.text = name
         }
 
     }
